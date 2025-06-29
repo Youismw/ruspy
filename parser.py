dee77103244fbf757896bfded03fd7e1c2c34ad3
Youismw <166497237+Youismw@users.noreply.github.com>
2025-06-29 18:37:27 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,19 @@
+# parser.py
+def parse(tokens):
+    it = iter(tokens)
+    ast = []
+    try:
+        while True:
+            kind, val = next(it)
+            if kind == 'ID':
+                next_kind, next_val = next(it)
+                if next_kind == 'ASSIGN':
+                    rhs_kind, rhs_val = next(it)
+                    ast.append(('assign', val, rhs_val))
+            elif kind == 'PRINT':
+                expr_kind, expr_val = next(it)
+                ast.append(('print', expr_val))
+    except StopIteration:
+        pass
+    return ast
 
